4cacf6789cf630a3348698824162b31df6120b9d
RajeshGurusamy <116284107+RajeshGurusamy@users.noreply.github.com>
2022-11-09 11:31:58 -0500

    initial commit

---
+++ src/main/java/com/persistent/consumer/SpringbootRabbitMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.persistent.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitMqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitMqConsumerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/persistent/consumer/config/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.persistent.consumer.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.persistent.consumer.service.RabbitMQConsumer;
+
+@Configuration
+public class RabbitMQConfig {
+
+	@Value("${payment.rabbitmq.queue}")
+	String queueName;
+	
+	@Value("${spring.rabbitmq.username}")
+	String username;
+
+	@Value("${spring.rabbitmq.password}")
+	private String password;
+	
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+	
+	@Bean
+	MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+		SimpleMessageListenerContainer simpleListenerContainer = new SimpleMessageListenerContainer();
+		simpleListenerContainer.setConnectionFactory(connectionFactory);
+		simpleListenerContainer.setQueues(queue());
+		simpleListenerContainer.setMessageListener(new RabbitMQConsumer());
+		return simpleListenerContainer;
+	}
+}
